fc9e03cdc5a5daf9137c347310685d0dd20c92b2
smarinskaya <svetlana.marinskaya@gmail.com>
2019-10-30 12:03:03 -0400

    Added drop downs

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,14 @@
 class Task
   include Mongoid::Document
 
-  field :task_name,       type: String, default: ""
-  field :target_username, type: String, default: ""
-  field :target_password, type: String, default: ""
-  field :source_username, type: String, default: ""
-  field :source_password, type: String, default: ""
-  field :source_pattern,  type: String, default: ""
-
+  field :task_name,          type: String, default: ""
+  field :target_type,        type: String, default: ""
+  field :target_file_format, type: String, default: ""
+  field :target_stream_type, type: String, default: ""
+  field :target_username,    type: String, default: ""
+  field :target_password,    type: String, default: ""
+  field :source_type,        type: String, default: ""
+  field :source_username,    type: String, default: ""
+  field :source_password,    type: String, default: ""
+  field :source_pattern,     type: String, default: ""
 end
