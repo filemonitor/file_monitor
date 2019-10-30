0ca2c1f54cf2bddaad97695d08b6ce440cfef008
Marc Bellingrath <marrrc.b@gmail.com>
2019-10-30 14:01:05 -0400

    Updates to jobs.

---
+++ app/jobs/file_copy_job.rb
@@ -0,0 +1,23 @@
+require 'iostreams'
+require 'net/sftp'
+
+class FileCopyJob < RocketJob::Job
+
+  self.description         = "File Copy Job"
+  self.destroy_on_complete = false
+  self.collect_output      = false  
+
+  field :task, type: Hash, user_editable: false
+  field :source_username, type: String, user_editable: false
+  field :source_password, type: String, user_editable: false
+  field :source_hostname, type: String, user_editable: false
+  
+  field :target_username, type: String, user_editable: false
+  field :target_password, type: String, user_editable: false
+  field :target_hostname, type: String, user_editable: false
+
+  def perform
+
+  end
+
+end
