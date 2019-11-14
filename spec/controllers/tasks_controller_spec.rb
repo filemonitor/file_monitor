3c2519ee3e269b786e228dcb3633c360c6aefe33
smarinskaya <svetlana.marinskaya@gmail.com>
2019-11-14 15:27:24 -0500

    removed comments

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -8,14 +8,12 @@ RSpec.describe TasksController, type: :controller do
     login_user
 
     it "should have a current_user" do
-      # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
       expect(subject.current_user).to_not eq(nil)
     end
 
     it "returns a success response" do
       get :index
       expect(response).to be_successful # be_successful expects a HTTP Status code of 200
-      #expect(response).to have_http_status(302) # Expects a HTTP Status code of 302
     end
   end
 end
