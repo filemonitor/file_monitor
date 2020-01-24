c2e3b91e53ebb80a4bea06ad2fb72d3333c3c48d
smarinskaya <svetlana.marinskaya@gmail.com>
2020-01-24 16:01:44 -0500

    Autoatic rubocop fixes

--- app/controllers/admin/monitor_controller.rb
+++ app/controllers/admin/monitor_controller.rb
@@ -5,7 +5,7 @@ module Admin
     skip_before_action :authenticate_user!
 
     def health_check
-      #Ensure the Mongo connection is active
+      # Ensure the Mongo connection is active
       User.count
       render body: "FileMonitor is alive at #{Time.current} from #{`hostname`.strip}."
     end
